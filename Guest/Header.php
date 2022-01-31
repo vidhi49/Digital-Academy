8dc09945b87149bfa66b36677e4e4efad8267a9c
Aarti <rtgohil111@gmail.com>
2022-01-31 19:22:12 +0530

    header

--- Guest/Header.php
+++ Guest/Header.php
@@ -13,7 +13,7 @@
 <body>
   <nav class="navbar navbar-expand-sm bg-dark navbar-dark py-1">
     <div class="container-fluid">
-      <img class='logo navbar-brand ms-5' src='../img/logo1.png' />
+      <img class='logo navbar-brand ms-5' src='../Images/logo1.png' />
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -23,21 +23,62 @@
             <a class="nav-link  " href="#">Home</a>
           </li>
           <li class="nav-item m-2">
-            <a class="nav-link " href="signup.php">Register</a>
+            <a class="nav-link " href="SignUp.php">Register</a>
           </li>
           <li class="nav-item m-2">
-            <a class="nav-link " href="login.php">Login</a>
+            <a class="nav-link " href="Login.php">Login</a>
           </li>
           <li class="nav-item m-2">
-            <a class="nav-link " href="aboutus.php">About Us</a>
+            <a class="nav-link " href="Aboutus.php">About Us</a>
           </li>
           <li class="nav-item m-2">
-            <a class="nav-link " href="contactus.php">Contact Us</a>
+            <a class="nav-link " href="Contactus.php">Contact Us</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
+  <div>
+    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner ">
+        <div class="carousel-item active" data-bs-interval="10000">
+          <img src="../Images/sl1.jpg" class="d-block img-carousel" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+          <img src="../Images/sl2.jpg" class="d-block img-carousel" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="../Images/sl3.jpg" class="d-block img-carousel" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
 </body>
 
 </html>
