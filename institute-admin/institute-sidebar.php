27434e23d37a1fd4569490fab55f61b1752bffd8
Aarti <rtgohil111@gmail.com>
2022-04-01 19:26:50 +0530

    student sidebar

--- institute-admin/institute-sidebar.php
+++ institute-admin/institute-sidebar.php
@@ -10,26 +10,26 @@ include("../connect.php");
 
 <head>
   <style>
-    /* * {
+  /* * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: "Poopins", sans-serif;
   } */
 
-    .institute-sidebar {
-      /* position: absolute; */
-      /* top: 0; */
-      /* left: 0; */
-      /* height: 100%; */
-      width: 230px;
-      background-color: #041562;
-      padding: 50px 0px 16px 0px;
-      /* padding: 6px 14px; */
-      /* transition: all 0.5s ease; */
-    }
+  .institute-sidebar {
+    /* position: absolute; */
+    /* top: 0; */
+    /* left: 0; */
+    /* height: 100%; */
+    width: 230px;
+    background-color: #041562;
+    padding: 50px 0px 16px 0px;
+    /* padding: 6px 14px; */
+    /* transition: all 0.5s ease; */
+  }
 
-    /* 
+  /* 
   .institute-sidebar.active {
     width: 75px;
   } */
@@ -37,9 +37,9 @@ include("../connect.php");
 
 
 
-    /* .institute-sidebar #btn {
+  /* .institute-sidebar #btn {
     /* position: absolute; */
-    /* color: #fff;
+  /* color: #fff;
     left: 90%;
     top: 6px;
     font-size: 20px;
@@ -48,158 +48,158 @@ include("../connect.php");
     text-align: center;
     line-height: 50px;
     transform: translate(-50%); */
-    /* } */
+  /* } */
 
-    /* .institute-sidebar ul {
+  /* .institute-sidebar ul {
       margin-top: 20px;
   list-style: none;
   padding-left: 10px;
    } */
 
-    .institute-sidebar ul li {
-      position: relative;
-      height: 60px;
-      width: 100%;
-      /* padding: 0; */
-      /* margin: 5 5 0 0; */
-      list-style: none;
-      line-height: 50px;
-      /* border-radius: 12px; */
-    }
+  .institute-sidebar ul li {
+    position: relative;
+    height: 60px;
+    width: 100%;
+    /* padding: 0; */
+    /* margin: 5 5 0 0; */
+    list-style: none;
+    line-height: 50px;
+    /* border-radius: 12px; */
+  }
 
-    .institute-sidebar .links_name {
-      /* opacity: 0; */
-      pointer-events: none;
-      /* transition: all 0.5s ease; */
-      display: none;
-    }
+  .institute-sidebar .links_name {
+    /* opacity: 0; */
+    pointer-events: none;
+    /* transition: all 0.5s ease; */
+    display: none;
+  }
 
-    .institute-sidebar ul li a {
-      color: #fff;
-      /* display: flex; */
-      /* align-items: center; */
-      text-decoration: none;
-      /* transition: all 0.4s ease; */
-      /* border-radius: 20px; */
-      /* border-top-left-radius: 20px; */
-      /* border-bottom-left-radius: 20px; */
-      /* white-space: nowrap; */
-    }
+  .institute-sidebar ul li a {
+    color: #fff;
+    /* display: flex; */
+    /* align-items: center; */
+    text-decoration: none;
+    /* transition: all 0.4s ease; */
+    /* border-radius: 20px; */
+    /* border-top-left-radius: 20px; */
+    /* border-bottom-left-radius: 20px; */
+    /* white-space: nowrap; */
+  }
 
-    .institute-sidebar ul li a:hover {
-      /* background: #fff; */
-      color: #fff;
-      font-size: 20px;
-      transition: all 0.5s ease;
-      /* margin-right: 8px; */
-    }
+  .institute-sidebar ul li a:hover {
+    /* background: #fff; */
+    color: #fff;
+    font-size: 20px;
+    transition: all 0.5s ease;
+    /* margin-right: 8px; */
+  }
 
-    .institute-sidebar ul li a i {
-      height: 50px;
-      min-width: 50px;
-      /* border-radius: 50px; */
-      text-align: center;
-    }
+  .institute-sidebar ul li a i {
+    height: 50px;
+    min-width: 50px;
+    /* border-radius: 50px; */
+    text-align: center;
+  }
 
-    /* li.active a i {
+  /* li.active a i {
     background: #041562;
     border: 2px solid #fff;
     color: #fff;
     /* border-radius: 50px; */
-    /* } */
+  /* } */
 
-    .institute-sidebar ul li.active {
-      background-color: #eee;
-      border-top-left-radius: 60px;
-      border-bottom-left-radius: 60px;
-    }
+  .institute-sidebar ul li.active {
+    background-color: #eee;
+    border-top-left-radius: 60px;
+    border-bottom-left-radius: 60px;
+  }
 
-    .institute-sidebar ul li.active a {
-      color: #11101d;
-    }
+  .institute-sidebar ul li.active a {
+    color: #11101d;
+  }
 
-    .institute-sidebar ul li.active a:hover {
-      color: #11101d;
-    }
+  .institute-sidebar ul li.active a:hover {
+    color: #11101d;
+  }
 
-    .institute-sidebar ul li.active a i {
-      height: 50px;
+  .institute-sidebar ul li.active a i {
+    height: 50px;
+    width: 50px;
+    /* background: #041562; */
+    border-radius: 50%;
+    border-bottom-left-radius: 50%;
+    border-top-left-radius: 50%;
+    /* color: #eee; */
+    /* margin: 5px 5px 5px 5px; */
+  }
+
+  .institute-sidebar ul li.active b:nth-child(1) {
+    position: absolute;
+    width: 100%;
+    height: 20px;
+    background: #eee;
+    /* border-bottom-right-radius: 60px; */
+    top: -20px;
+    left: 0;
+  }
+
+  .institute-sidebar ul li.active b:nth-child(1)::after {
+    position: absolute;
+    content: '';
+    width: 100%;
+    height: 20px;
+    background: #041562;
+    border-bottom-right-radius: 60px;
+    /* top: -20px; */
+    left: 0;
+  }
+
+  .institute-sidebar ul li.active b:nth-child(2) {
+    position: absolute;
+    width: 100%;
+    height: 20px;
+    background: #eee;
+    bottom: -20px;
+    left: 0;
+  }
+
+  .institute-sidebar ul li.active b:nth-child(2)::after {
+    position: absolute;
+    content: '';
+    width: 100%;
+    height: 20px;
+    background: #041562;
+    border-top-right-radius: 60px;
+    left: 0;
+    /* bottom: -30px; */
+  }
+
+  .institute-sidebar.active~.content {
+    /* width: calc(100% -75px); */
+    /* left: 30px; */
+    transition: all 0.3s ease;
+  }
+
+  @media only screen and (max-width: 500px) {
+
+    .insttitute-sidebar {
       width: 50px;
-      /* background: #041562; */
-      border-radius: 50%;
-      border-bottom-left-radius: 50%;
-      border-top-left-radius: 50%;
-      /* color: #eee; */
-      /* margin: 5px 5px 5px 5px; */
+      align-items: center;
+      text-align: center;
+      display: block;
     }
 
-    .institute-sidebar ul li.active b:nth-child(1) {
-      position: absolute;
-      width: 100%;
-      height: 20px;
-      background: #eee;
-      /* border-bottom-right-radius: 60px; */
-      top: -20px;
-      left: 0;
+    .institute-content {
+      width: calc(100vw - 50px);
+      padding: 15px;
+      margin: 0px;
+
     }
 
-    .institute-sidebar ul li.active b:nth-child(1)::after {
-      position: absolute;
-      content: '';
-      width: 100%;
-      height: 20px;
-      background: #041562;
-      border-bottom-right-radius: 60px;
-      /* top: -20px; */
-      left: 0;
+    .insttitute-sidebar .links_name {
+      display: none;
     }
-
-    .institute-sidebar ul li.active b:nth-child(2) {
-      position: absolute;
-      width: 100%;
-      height: 20px;
-      background: #eee;
-      bottom: -20px;
-      left: 0;
-    }
-
-    .institute-sidebar ul li.active b:nth-child(2)::after {
-      position: absolute;
-      content: '';
-      width: 100%;
-      height: 20px;
-      background: #041562;
-      border-top-right-radius: 60px;
-      left: 0;
-      /* bottom: -30px; */
-    }
-
-    .institute-sidebar.active~.content {
-      /* width: calc(100% -75px); */
-      /* left: 30px; */
-      transition: all 0.3s ease;
-    }
-
-    @media only screen and (max-width: 500px) {
-
-      .insttitute-sidebar {
-        width: 50px;
-        align-items: center;
-        text-align: center;
-        display: block;
-      }
-
-      .institute-content {
-        width: calc(100vw - 50px);
-        padding: 15px;
-        margin: 0px;
-
-      }
-
-      .insttitute-sidebar .links_name {
-        display: none;
-      }
-    }
+  }
   </style>
 </head>
 
@@ -230,7 +230,7 @@ include("../connect.php");
         <b></b>
         <b></b>
         <a href="#" class="nav-link">
-          
+
           <i class='fa fa-check'></i>
           <span class="links_name d-sm-inline">Dashbord</span>
         </a>
@@ -343,7 +343,7 @@ include("../connect.php");
 
       <li <?php if (($a == 'viewclassattedance')) {
             echo "class='list active nav-item '";
-          } ?>  >
+          } ?>>
         <b></b>
         <b></b>
         <a href="viewclassattedance.php" class="nav-link">
@@ -354,7 +354,7 @@ include("../connect.php");
       </li>
       <li <?php if (($a == 'viewstudentattedance')) {
             echo "class='list active nav-item '";
-          } ?>  >
+          } ?>>
         <b></b>
         <b></b>
         <a href="viewstudentattedance.php" class="nav-link">
