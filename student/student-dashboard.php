954268486a15d111eb1a4194519f64816b2b89af
Aarti <rtgohil111@gmail.com>
2022-04-21 19:16:36 +0530

    student material count

--- student/student-dashboard.php
+++ student/student-dashboard.php
@@ -26,6 +26,7 @@ $(document).ready(function() {
   <div class="d-flex">
     <?php include("student-sidebar.php"); ?>
     <div class="student-content mt-2 ">
+
       <div class="row shadow mx-5" style="border-radius: 20px;background-color:#fffdd0">
         <div class="col-sm-8 d-flex align-items-center">
           <div class="m-5 " style="line-height: 50px;">
@@ -93,162 +94,105 @@ $(document).ready(function() {
 
 
       <div class="row  mt-5 ">
-        <?php
-        // include("admin-sidebar.php");
-        $date = date('Y-m-d');
-        $q = "select * from exam_tbl where Inst_id='$inst_id' and Class_id='$row[15]' and Exam_Date > curdate()+7";
-        $res = mysqli_query($con, $q) or die("Query Failed");
-        $nor = mysqli_num_rows($res);
-        if ($nor > 0) {
-        ?>
-
-        <div class="col">
-          <div class="row mx-2 fs-4">
-            <p class="">Exams</p>
-            <div class="col d-flex justify-content-end">
-              <a href="studentExam.php">View All</a>
-            </div>
-          </div>
+        <div class="col-sm-8">
           <?php
+          // include("admin-sidebar.php");
+          $date = date('Y-m-d');
+          $q = "select * from exam_tbl where Inst_id='$inst_id' and Class_id='$row[15]' and  Status='Created' ";
+          // echo $q;
 
-          while ($r = mysqli_fetch_array($res)) {
-
-
-            $q1 = "select * from subject_tbl where Id='$r[5]'";
-            $result1 = mysqli_query($con, $q1);
-            $re1 = mysqli_fetch_array($result1);
-            echo '
-                        
-                              <div class="col mt-3">
-                                <div class="card-body shadow" style="border-radius: 0px;background-color:#d0f0c0">
-                                  <div class="row">
-                                    <div class="col-sm-4 d-flex align-items-center">
-                                      <div class=" d-flex align-items-center">
-                                        <i class="fa fa-bookmark fs-4 pr-4" aria-hidden="true"></i>
-                                        <h4> ' . $re1[3] . '</h4>
-                                      </div>
-                                    </div>
-                                    <div class="col d-flex align-items-center justify-content-end">
-                                      <h4> ' . $r[6] . '</h4>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                        ';
-          }
-        } else {
-        }
+          $res = mysqli_query($con, $q) or die("Query Failed");
+          $nor = mysqli_num_rows($res);
+          if ($nor > 0) {
           ?>
 
+          <div class="row">
+            <div class="col-sm-6">
+              <h2 class="d-flex justify-content-start mb-3">Exams</h2>
+            </div>
+            <div class="col-sm-6">
+              <a href="studentExam.php" class="d-flex justify-content-end mb-3">View All</a>
+            </div>
+          </div>
+
+          <?php
+
+            while ($r = mysqli_fetch_array($res)) {
+
+              $q1 = "select * from subject_tbl where Id='$r[5]'";
+              $result1 = mysqli_query($con, $q1);
+              $re1 = mysqli_fetch_array($result1);
+              echo '
+                  <div class="card-body shadow" style="border-radius: 0px;background-color:#d0f0c0">
+                    <div class="row">
+                      <div class="col-sm-6 d-flex align-items-center">
+                        <div class=" d-flex align-items-center">
+                          <i class="fa fa-bookmark fs-4 pr-4" aria-hidden="true"></i>
+                          <h4> ' . $re1[3] . '</h4>
+                        </div>
+                      </div>
+                      <div class="col-sm-6 d-flex align-items-center justify-content-end">
+                        <h4> ' . $r[6] . '</h4>
+                      </div>
+                    </div>
+                  </div>
+                        ';
+            }
+          } else {
+          }
+          ?>
         </div>
-        <div class="col-sm-4 p-4 mx-2 shadow" style="border-radius: 20px;background-color:#f0ffff">
+        <div class="col-sm-4 p-4 shadow mt-5" style="border-radius: 20px;background-color:#f0ffff">
           <h3>Students of <?php echo $row[13] . '-' . $row[14] ?></h3>
           <div>
             <canvas id="barChart"></canvas>
           </div>
         </div>
-
       </div>
-      <div class="row " style="padding-top: 10px;">
-
-        <div class="col p-4 mx-2 shadow" style="border-radius: 20px;background-color:#f0ffff">
-          <table class="table table-flush table-hover responsive ">
-            <div class="row mx-2 fs-4">
-              <div class="col ">
-                <p class="">Student List</p>
-              </div>
-              <div class="col d-flex justify-content-end">
-                <a href="student.php">View All</a>
-              </div>
+      <div class="mt-5 w-50" style="padding-top: 10px;">
+        <div class="p-4 shadow" style="border-radius: 20px;background-color:#f0ffff">
+          <div class="row fs-4">
+            <div class="col-sm-6 ">
+              <p class="">Material Uploaded</p>
             </div>
-            <tbody>
-              <?php
-              $que = "select * from student_tbl where  Class_id='$row[15]' Limit 5 ";
-              $result = mysqli_query($con, $que);
-              $cnt = 0;
-              while ($r = mysqli_fetch_array($result)) {
-                $cnt = $cnt + 1;
-                echo "<tr>
-                                       <td>$cnt</td>
-                                       <td>
-                                        <img class='popup' src='student_profile/$r[6]' alt='image'  height='50' width='50'>
-                                        </td>
-                                        <td>$r[4]</td>
-                                       </tr>";
-              }
-
-              ?>
-
-            </tbody>
-          </table>
-        </div>
-        <div class="col p-4 mx-2 shadow" style="border-radius: 20px;background-color:#f0ffff">
-
-          <table class="table table-flush table-hover responsive ">
-            <div class="row mx-2 fs-4">
-              <div class="col ">
-                <p class="">Material Uploaded</p>
-              </div>
-              <div class="col d-flex justify-content-end">
-                <a href="subjectMaterial.php">View All</a>
-              </div>
+            <div class="col-sm-6 d-flex justify-content-end">
+              <a href="subjectMaterial.php">View All</a>
             </div>
-            <tbody>
-              <?php
-              $q = "select * from student_tbl where  Id='$Id' and Inst_id='$inst_id'";
-              $res = mysqli_query($con, $q);
-              // echo $q;
-              $nor = mysqli_num_rows($res);
-              while ($r = mysqli_fetch_array($res)) {
-                $q6 = "select * from class_tbl where Insti_id='$inst_id' and Id='$r[15]'";
-                // echo $q6;
-                $resc = mysqli_query($con, $q6);
-                $rs3 = mysqli_fetch_array($resc);
-
-                $q2 = "select * from class_tbl where Insti_id='$inst_id' and Name='$rs3[2]' and Section='A'";
-                // echo $q2;
-                $res2 = mysqli_query($con, $q2);
-                $rs2 = mysqli_fetch_array($res2);
+          </div>
+          <div class="table-responsive">
+            <table class="table table-flush table-hover responsive ">
+              <tbody>
+                <?php
+                $q = "select * from student_tbl where Id='$Id' and Inst_id='$inst_id'";
+                // echo $q;
+                $rs = mysqli_query($con, $q);
+                $r = mysqli_fetch_array($rs);
 
 
-                $c = "select * from subject_tbl where Class_id='$rs2[0]' and Inst_id='$inst_id'";
-                // echo $c;
-                $res1 = mysqli_query($con, $c);
-                while ($rs = mysqli_fetch_array($res1)) {
-              ?>
 
-              <div class="row">
-                <div class="col">
-                  <?php echo $rs[3]; ?>
-                </div>
-                <!-- <div class="col d-flex justify-content-end"> -->
-                <!-- <li class="fa fa-bookmark fs-1 ms-5 text-dark"></li>-->
-                <div class="col d-flex justify-content-end p-3 bg-light" style="border-radius: 50px;">
-                  <!-- <?php
+                $s = "select Subject_Id,count(*) from upload_tbl where Inst_id='$inst_id' and Class_id='$r[15]' group by Subject_Id";
+                // echo $s;
+                $res = mysqli_query($con, $s);
 
-                            // $que = mysqli_query($con, "select count(*) from studentMaterialList_tbl where Class_id='$row[15]' and Inst_id='$inst_id' and  Subject_Id='$subid'group by Subject_id");
-                            // $result2 = mysqli_fetch_array($que);
-                            // echo "<b class='p-3 fs-1'>$result2[0]</b>";
-                            ?> -->
-                </div>
-              </div>
+                while ($re = mysqli_fetch_array($res)) {
+
+                  $sub = "select * from subject_tbl where Id='$re[0]' and Inst_id='$inst_id'";
+                  // echo $sub;
+                  $res1 = mysqli_query($con, $sub);
+                  $rq = mysqli_fetch_array($res1);
+
+                  // echo $re[1], $re[0];
+                  echo "<tr>
+                  <td>$rq[3]</td>
+                  <td class='d-flex justify-content-end'>$re[1]</td></tr>";
+                }
+                ?>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  </a>
-
-  <?php
-                }
-              }
-              // echo $q; 
-?>
-  </tbody>
-  </table>
-  </div>
-  </div>
-  </div>
-  </div>
   </div>
 
 </body>
